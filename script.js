f8596ca9744c98418ade6adc4c1d785bab7accf7
Antony TR <antonyetr@outlook.com>
2023-12-18 23:57:48 -0600

    symbol selectors added with eventlisteners

--- script.js
+++ script.js
@@ -10,7 +10,19 @@ const winCombinations = [
   [2, 4, 6],
 ];
 
-let player = "x";
+let player;
+
+const SYMBOL_BUTTON = Array.from(
+  document.getElementsByClassName("symbol-button")
+);
+
+SYMBOL_BUTTON.forEach((btn) => {
+  btn.addEventListener("click", () => assignPlayerSymbol(btn));
+});
+
+function assignPlayerSymbol(button) {
+  player = button.innerHTML;
+}
 
 function checkWinner() {
   for (const combination of winCombinations) {
@@ -23,18 +35,22 @@ function checkWinner() {
 }
 
 function move(index) {
-  if (board[index] === "") {
-    board[index] = player;
-
-    if (checkWinner()) {
-      console.log("Ganaste");
-    } else {
-      player = player === "x" ? "o" : "x";
-    }
+  if (!player) {
+    console.log("Pick one player");
+    return;
   } else {
-    console.log("Casilla Ocupada");
-  }
+    if (board[index] === "") {
+      board[index] = player;
 
+      if (checkWinner()) {
+        console.log("Ganaste");
+      } else {
+        player = player === "X" ? "O" : "X";
+      }
+    } else {
+      console.log("Casilla Ocupada");
+    }
+  }
   console.log(`${board[0]} ${board[1]} ${board[2]}\n${board[3]} ${board[4]} ${board[5]}\n${board[6]} ${board[7]} ${board[8]}
   `);
 }
