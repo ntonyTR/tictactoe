06e111dc4158da79e7d7e43868d19b53668f405b
Antony TR <antonyetr@outlook.com>
2023-12-20 22:33:36 -0600

    Encapsulated the game logic within a 'game' object

--- script.js
+++ script.js
@@ -1,56 +1,84 @@
-const board = ["", "", "", "", "", "", "", "", ""];
-const winCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-let player;
-
-const SYMBOL_BUTTON = Array.from(
-  document.getElementsByClassName("symbol-button")
-);
-
-SYMBOL_BUTTON.forEach((btn) => {
-  btn.addEventListener("click", () => assignPlayerSymbol(btn));
-});
-
-function assignPlayerSymbol(button) {
-  player = button.innerHTML;
-}
-
-function checkWinner() {
-  for (const combination of winCombinations) {
-    const [a, b, c] = combination;
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return true;
+const game = {
+  currentPlayer: "",
+  board: ["", "", "", "", "", "", "", "", ""],
+  winCombinations: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ],
+  gameMessages: {
+    win: (winner) => {
+      console.log(`${winner} wins.`); //TEST
+    },
+    tie: () => {
+      console.log("Tie"); //TEST
+    },
+    selectSymbol: () => {
+      console.log("Select a symbol."); //TEST
+    },
+    selectAnotherCell: () => {
+      console.log("Select another cell."); //TEST
+    },
+  },
+  play: function (i) {
+    if (!this.currentPlayer) {
+      this.gameMessages.selectSymbol();
+      return
     }
-  }
-  return false;
-}
 
-function move(index) {
-  if (!player) {
-    console.log("Pick one player");
-    return;
-  } else {
-    if (board[index] === "") {
-      board[index] = player;
+    if (this.isValidMove(i)) {
+      this.makeMove(i);
 
-      if (checkWinner()) {
-        console.log("Ganaste");
-      } else {
-        player = player === "X" ? "O" : "X";
+      if (this.isWinner()) {
+        this.gameMessages.win(this.currentPlayer);
+        return;
       }
-    } else {
-      console.log("Casilla Ocupada");
+
+      if (this.isBoardFull()) {
+        this.gameMessages.tie();
+        return;
+      }
+
+      this.switchPlayer();
+      return;
     }
-  }
-  console.log(`${board[0]} ${board[1]} ${board[2]}\n${board[3]} ${board[4]} ${board[5]}\n${board[6]} ${board[7]} ${board[8]}
-  `);
-}
+    this.gameMessages.selectAnotherCell();
+  },
+  makeMove: function (i) {
+    this.board[i] = this.currentPlayer;
+    console.log(`
+    ${this.board[0]} | ${this.board[1]} | ${this.board[2]}
+    ----------- 
+    ${this.board[3]} | ${this.board[4]} | ${this.board[5]}
+    ----------- 
+    ${this.board[6]} | ${this.board[7]} | ${this.board[8]}
+   `); // TEST
+  },
+  isValidMove: function (i) {
+    return this.board[i] === "";
+  },
+  switchPlayer: function () {
+    this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+  },
+  isWinner: function () {
+    for (const combination of this.winCombinations) {
+      const [a, b, c] = combination;
+      if (
+        this.board[a] &&
+        this.board[a] === this.board[b] &&
+        this.board[a] === this.board[c]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  },
+  isBoardFull: function () {
+    return this.board.every((cell) => cell !== "");
+  },
+};
